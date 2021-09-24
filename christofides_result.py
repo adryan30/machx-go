542a2dae20f00c77cfd3d2445ed6defb52342b1f
Adryan Almeida <adryan1990@gmail.com>
2021-09-24 13:07:50 -0300

    adds lau15 graph

--- christofides_result.py
+++ christofides_result.py
@@ -1,9 +1,9 @@
 from christofides_code import tsp
 from timeit import default_timer as timer
-from graphs import sh07, sp11, uk12, wg22,  wg59, sgb128
+from graphs import sh07, sp11, uk12, lau15, wg22,  wg59, sgb128
 
 start = timer()
-result = tsp(sgb128)
+result = tsp(lau15)
 end = timer()
 print(["Christofides", result[0], result[1]])
 print(f'{end - start}s')
