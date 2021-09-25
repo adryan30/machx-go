ca089f4a9aa17c76bfb6cd9e176947be58dc78da
Adryan Almeida <adryan1990@gmail.com>
2021-09-25 10:57:57 -0300

    adds final usca312

--- christofides_result.py
+++ christofides_result.py
@@ -1,6 +1,7 @@
 from christofides_code import tsp
 from timeit import default_timer as timer
-from graphs import sh07, sp11, uk12, lau15, wg22,  wg59, sgb128, usca312
+from graphs import sh07, sp11, uk12, lau15, wg22,  wg59, sgb128
+from datasets.list import usca312
 
 start = timer()
 result = tsp(usca312)
--- datasets/dataset_to_list.py
+++ datasets/dataset_to_list.py
@@ -1,6 +1,15 @@
 import re
 
+
+def chunks(lst, n):
+    """Yield successive n-sized chunks from lst."""
+    for i in range(0, len(lst), n):
+        yield lst[i:i + n]
+
+
 f = open("./usca312.txt", "r")
 lines = f.read()
 matches = re.findall(r"\d", lines)
-print(matches)
+numbers = list(map(int, matches))
+chunks_list = list(chunks(numbers, 312))
+print(chunks_list)
